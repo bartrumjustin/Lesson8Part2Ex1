8205cbe516a6089ced8bcafeb804aa40f686ce10
bartrumjustin <bartrumjustin@gmail.com>
2026-03-05 20:28:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson8Part2Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
